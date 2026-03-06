1cf3645d9012426c1de72c63968cc40933210400
Aditya Kumar <adityaayush1712@gmail.com>
2026-03-07 01:17:36 +0530

    vercel backend setup with change in server.js listen port change

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+import app from "./src/app.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.join(__dirname, ".env") });
 
-import app from "./src/app.js";
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
